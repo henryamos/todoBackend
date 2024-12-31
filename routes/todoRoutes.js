08adfbd15ccdfee033eb07938e10df28146b31eb
Henry Amos <amoshenry95@gmail.com>
2024-12-31 12:44:33 +0000

    ✅ test(validation): update validation and create separate validation for updates and adding todos ✨ feat(validation): add separate validation for updating and adding todos ♻️ refactor(validation): separate validation for adding and updating todos

--- routes/todoRoutes.js
+++ routes/todoRoutes.js
@@ -1,13 +1,22 @@
-
-import { createTodo, deleteTodo, getTodoById, getTodos, updateTodo } from "../controllers/todoController.js";
+import {
+  createTodo,
+  deleteTodo,
+  getTodoById,
+  getTodos,
+  updateTodo,
+} from "../controllers/todoController.js";
 import { Router } from "express";
-import { validateTodo } from "../validations/todoValidation.js";
+import {
+  validateCreateTodo,
+  validateUpdateTodo,
+} from "../validations/todoValidation.js";
+
 const router = Router();
 
-router.get("/",getTodos);
+router.get("/", getTodos);
 router.get("/:id", getTodoById);
-router.post("/", validateTodo, createTodo);
+router.post("/", validateCreateTodo, createTodo);
+router.patch("/:id", validateUpdateTodo, updateTodo);
 router.delete("/:id", deleteTodo);
-router.patch("/:id", validateTodo,updateTodo);
 
 export default router;
--- validations/todoValidation.js
+++ validations/todoValidation.js
@@ -1,7 +1,6 @@
-
 import { body, validationResult } from "express-validator";
 
-export const validateTodo = [
+export const validateCreateTodo = [
   body("title")
     .isLength({ min: 3 })
     .withMessage("Title must be at least 3 characters long"),
@@ -21,3 +20,25 @@ export const validateTodo = [
   },
 ];
 
+export const validateUpdateTodo = [
+  body("title")
+    .optional()
+    .isLength({ min: 3 })
+    .withMessage("Title must be at least 3 characters long"),
+  body("description")
+    .optional()
+    .isLength({ min: 5 })
+    .withMessage("Description must be at least 5 characters long"),
+  body("dueDate")
+    .optional()
+    .isISO8601()
+    .toDate()
+    .withMessage("Due date must be a valid date"),
+  (req, res, next) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    next();
+  },
+];
