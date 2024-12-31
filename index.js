e4c43b0a7a579f15ce67f7eb30bc0230c49d6465
Henry Amos <amoshenry95@gmail.com>
2024-12-31 12:49:05 +0000

    🐛 fix(cors): resolve CORS error ✅ fix(security): handle CORS issue 🚀 perf(cors): improve CORS configuration

--- index.js
+++ index.js
@@ -2,12 +2,14 @@ import express from "express";
 import mongoose from "mongoose";
 import todoRoutes from "./routes/todoRoutes.js";
 import bodyParser from "body-parser";
+import cors from "cors";
 import { configDotenv } from "dotenv";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
+app.use(cors());
  app.use('/api/todos', todoRoutes);
 configDotenv()
 mongoose
